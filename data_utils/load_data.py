834c4be1af074848e532324264fc9edbe195de78
Shane Soh <shanesoh@gmail.com>
2016-06-04 16:32:27 +0800

    Refactored code; added edit distance features

---
+++ data_utils/load_data.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+from nltk.metrics.distance import edit_distance
+
+
+def _add_item_features(items):
+    """
+    Do feature engineering here by adding features to each individual item, i.e.
+    features specific to each item
+    :param items: iteminfo df
+    """
+    # Add lens of texts
+    items['len_title'] = items['title'].str.len()
+    items['len_description'] = items['description'].str.len()
+    items['len_attrsJSON'] = items['attrsJSON'].str.len()
+
+    return items
+
+
+def _add_pairs_features(pairs):
+    """
+    Do feature engineering here by adding features to each item pairs, i.e.
+    features common to both items in the pair
+    :param pairs: itempairs df
+    """
+    # Add binary features indicating if attributes are equal
+    pairs['price_same'] = np.equal(
+        pairs['price_1'],
+        pairs['price_2']).astype(
+        np.int32)
+    pairs['locationID_same'] = np.equal(
+        pairs['locationID_1'],
+        pairs['locationID_2']).astype(
+        np.int32)
+    pairs['categoryID_same'] = np.equal(
+        pairs['categoryID_1'],
+        pairs['categoryID_2']).astype(
+        np.int32)
+    pairs['regionID_same'] = np.equal(
+        pairs['regionID_1'],
+        pairs['regionID_2']).astype(
+        np.int32)
+    pairs['metroID_same'] = np.equal(
+        pairs['metroID_1'],
+        pairs['metroID_2']).astype(
+        np.int32)
+    pairs['lat_same'] = np.equal(
+        pairs['lat_1'],
+        pairs['lat_2']).astype(
+        np.int32)
+    pairs['lon_same'] = np.equal(
+        pairs['lon_1'],
+        pairs['lon_2']).astype(
+        np.int32)
+
+    # Normalized edit distance of texts
+    pairs['title_dist'] = pairs[['title_1', 'title_2']].apply(
+        lambda x: edit_distance(x[0], x[1]) / float(len(x[0]) + len(x[1])),
+        axis=1)
+    pairs['description_dist'] = pairs[['description_1', 'description_2']].apply(
+        lambda x: edit_distance(x[0], x[1]) / float(len(x[0]) + len(x[1])),
+        axis=1)
+    pairs['attrsJSON_dist'] = pairs[['attrsJSON_1', 'attrsJSON_2']].apply(
+        lambda x: edit_distance(x[0], x[1]) / float(len(x[0]) + len(x[1])),
+        axis=1)
+    pairs.drop(['title_1', 'title_2', 'description_1', 'description_2',
+                'attrsJSON_1', 'attrsJSON_2'], axis=1, inplace=True)
+
+    return pairs
+
+
+def load_data():
+    """
+    Load ItemInfo, do feature engineering on ItemInfo, then merge into ItemPairs,
+    do feature engineering on ItemPairs, then return train/test examples and
+    features used
+    :return: train df, test df and list of features
+    """
+    types = {
+        'itemID': np.dtype(int),
+        'categoryID': np.dtype(int),
+        'title': np.dtype(str),
+        'description': np.dtype(str),
+        'images_array': np.dtype(str),
+        'attrsJSON': np.dtype(str),
+        'price': np.dtype(float),
+        'locationID': np.dtype(int),
+        'metroID': np.dtype(float),
+        'lat': np.dtype(float),
+        'lon': np.dtype(float),
+    }
+    print("Load ItemInfo_train.csv")
+    iteminfo_train = pd.read_csv("./data/ItemInfo_train.csv", dtype=types)
+    iteminfo_train.fillna(-1, inplace=True)
+
+    print("Load ItemInfo_test.csv")
+    iteminfo_test = pd.read_csv("./data/ItemInfo_test.csv", dtype=types)
+    iteminfo_test.fillna(-1, inplace=True)
+
+    # Add in location and category data
+    location = pd.read_csv("./data/Location.csv")
+    category = pd.read_csv("./data/Category.csv")
+    iteminfo_train = pd.merge(
+        iteminfo_train,
+        category,
+        how='left',
+        on='categoryID',
+        left_index=True)
+    iteminfo_train = pd.merge(
+        iteminfo_train,
+        location,
+        how='left',
+        on='locationID',
+        left_index=True)
+    iteminfo_test = pd.merge(
+        iteminfo_test,
+        category,
+        how='left',
+        on='categoryID',
+        left_index=True)
+    iteminfo_test = pd.merge(
+        iteminfo_test,
+        location,
+        how='left',
+        on='locationID',
+        left_index=True)
+
+    # Feature engineering on ItemInfo, i.e. individual items
+    iteminfo_train = _add_item_features(iteminfo_train)
+    iteminfo_test = _add_item_features(iteminfo_test)
+
+    # Merge iteminfo onto itempairs
+    itempairs_train, itempairs_test = _merge_itempairs(
+        iteminfo_train, iteminfo_test)
+
+    # Feature engineering on ItemPairs, i.e. pair of items
+    itempairs_train = _add_pairs_features(itempairs_train)
+    itempairs_test = _add_pairs_features(itempairs_test)
+
+    # Extract features
+    features = _get_features(itempairs_train, itempairs_test)
+    return itempairs_train, itempairs_test, features
+
+
+def _merge_itempairs(iteminfo_train, iteminfo_test):
+    types = {
+        'itemID_1': np.dtype(int),
+        'itemID_2': np.dtype(int),
+        'isDuplicate': np.dtype(int),
+        'generationMethod': np.dtype(int),
+    }
+    print("Load ItemPairs_train.csv")
+    itempairs_train = pd.read_csv("./data/ItemPairs_train.csv", dtype=types)
+    print("Load ItemPairs_test.csv")
+    itempairs_test = pd.read_csv("./data/ItemPairs_test.csv", dtype=types)
+
+    # Merge itempairs for train set
+    iteminfo_train_1 = iteminfo_train.copy(deep=True)
+    iteminfo_train_1.rename(columns=lambda x: x+'_1', inplace=True)
+    itempairs_train = pd.merge(itempairs_train, iteminfo_train_1, how='left',
+                               on='itemID_1', left_index=True)
+    iteminfo_train_2 = iteminfo_train.copy(deep=True)
+    iteminfo_train_2.rename(columns=lambda x: x+'_2', inplace=True)
+    itempairs_train = pd.merge(itempairs_train, iteminfo_train_2, how='left',
+                               on='itemID_2', left_index=True)
+    # 'generationMethod' exists only in train, so drop
+    itempairs_train.drop('generationMethod', axis=1, inplace=True)
+
+    # Merge itempairs for test set
+    iteminfo_test_1 = iteminfo_test.copy(deep=True)
+    iteminfo_test_1.rename(columns=lambda x: x+'_1', inplace=True)
+    itempairs_test = pd.merge(itempairs_test, iteminfo_test_1, how='left',
+                              on='itemID_1', left_index=True)
+    iteminfo_test_2 = iteminfo_test.copy(deep=True)
+    iteminfo_test_2.rename(columns=lambda x: x+'_2', inplace=True)
+    itempairs_test = pd.merge(itempairs_test, iteminfo_test_2, how='left',
+                              on='itemID_2', left_index=True)
+
+    return itempairs_train, itempairs_test
+
+
+def _get_features(train, test):
+    trainval = list(train.columns.values)
+    testval = list(test.columns.values)
+    output = list(set(trainval) & set(testval))
+    output.remove('itemID_1')
+    output.remove('itemID_2')
+    return output
---
+++ runscript.py
@@ -0,0 +1,131 @@
+"""
+Adapted from 'ZFTurbo: https://kaggle.com/zfturbo'
+"""
+__author__ = 'Shane Soh'
+
+from data_utils.load_data import load_data
+
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import roc_auc_score
+import xgboost as xgb
+import time
+import datetime
+from operator import itemgetter
+
+
+def create_feature_map(features):
+    outfile = open('xgb.fmap', 'w')
+    for i, feat in enumerate(features):
+        outfile.write('{0}\t{1}\tq\n'.format(i, feat))
+    outfile.close()
+
+
+def get_importance(gbm, features):
+    create_feature_map(features)
+    importance = gbm.get_fscore(fmap='xgb.fmap')
+    importance = sorted(importance.items(), key=itemgetter(1), reverse=True)
+    return importance
+
+
+def run_default_test(train, test, features, target, random_state=0):
+    eta = 0.1
+    max_depth = 5
+    subsample = 0.8
+    colsample_bytree = 0.8
+    start_time = time.time()
+
+    print(
+        'XGBoost params. ETA: {}, MAX_DEPTH: {}, SUBSAMPLE: {}, COLSAMPLE_BY_TREE: {}'.format(
+            eta,
+            max_depth,
+            subsample,
+            colsample_bytree))
+    params = {
+        "objective": "binary:logistic",
+        "booster": "gbtree",
+        "eval_metric": "auc",
+        "eta": eta,
+        "max_depth": max_depth,
+        "subsample": subsample,
+        "colsample_bytree": colsample_bytree,
+        "silent": 1,
+        "seed": random_state
+    }
+    num_boost_round = 260
+    early_stopping_rounds = 20
+    test_size = 0.1
+
+    X_train, X_valid = train_test_split(
+        train, test_size=test_size, random_state=random_state)
+    y_train = X_train[target]
+    y_valid = X_valid[target]
+    dtrain = xgb.DMatrix(X_train[features], y_train)
+    dvalid = xgb.DMatrix(X_valid[features], y_valid)
+
+    watchlist = [(dtrain, 'train'), (dvalid, 'eval')]
+    gbm = xgb.train(
+        params,
+        dtrain,
+        num_boost_round,
+        evals=watchlist,
+        early_stopping_rounds=early_stopping_rounds,
+        verbose_eval=True)
+
+    print("Validating...")
+    check = gbm.predict(
+        xgb.DMatrix(
+            X_valid[features]),
+        ntree_limit=gbm.best_ntree_limit)
+    score = roc_auc_score(X_valid[target].values, check)
+    print('Check error value: {:.6f}'.format(score))
+
+    imp = get_importance(gbm, features)
+    print('Importance array: ', imp)
+
+    print("Predict test set...")
+    test_prediction = gbm.predict(
+        xgb.DMatrix(
+            test[features]),
+        ntree_limit=gbm.best_ntree_limit)
+
+    print(
+        'Training time: {} minutes'.format(
+            round(
+                (time.time() - start_time)/60,
+                2)))
+    return test_prediction.tolist(), score
+
+
+def create_submission(score, test, prediction):
+    # Make Submission
+    now = datetime.datetime.now()
+    sub_file = 'submission_' + \
+        str(score) + '_' + str(now.strftime("%Y-%m-%d-%H-%M")) + '.csv'
+    print('Writing submission: ', sub_file)
+    f = open(sub_file, 'w')
+    f.write('id,probability\n')
+    total = 0
+    for id in test['id']:
+        str1 = str(id) + ',' + str(prediction[total])
+        str1 += '\n'
+        total += 1
+        f.write(str1)
+    f.close()
+
+if __name__ == '__main__':
+    train, test, features = load_data()
+    train.fillna(-1, inplace=True)
+    test.fillna(-1, inplace=True)
+    print('Length of train: ', len(train))
+    print('Length of test: ', len(test))
+    print('Features [{}]: {}'.format(len(features), sorted(features)))
+    # Get only subset of data
+    if 1:
+        len_old = len(train.index)
+        train = train.sample(frac=0.5)
+        len_new = len(train.index)
+        print('Reduce train from {} to {}'.format(len_old, len_new))
+    test_prediction, score = run_default_test(
+        train, test, features, 'isDuplicate')
+    print('Real score = {}'.format(score))
+    create_submission(score, test, test_prediction)
